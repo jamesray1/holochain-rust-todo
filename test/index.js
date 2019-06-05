fdc10e7081488a140a20a415856bafb76c00790e
James Ray <16969914+jamesray1@users.noreply.github.com>
2019-06-05 12:26:53 +1000

    Fix: add tags:

--- test/index.js
+++ test/index.js
@@ -1,20 +1,42 @@
 // This test file uses the tape testing framework.
 // To learn more, go here: https://github.com/substack/tape
-const { Config, Scenario } = require("@holochain/holochain-nodejs")
-Scenario.setTape(require("tape"))
-
+const { Config, Scenario } = require('@holochain/holochain-nodejs')
+Scenario.setTape(require('tape'))
 const dnaPath = "./dist/holochain-rust-todo.dna.json"
-const agentAlice = Config.agent("alice")
-const dna = Config.dna(dnaPath)
+const dna = Config.dna(dnaPath, 'happs')
+const agentAlice = Config.agent('alice')
 const instanceAlice = Config.instance(agentAlice, dna)
 const scenario = new Scenario([instanceAlice])
 
-scenario.runTape("description of example test", async (t, { alice }) => {
-  // Make a call to a Zome function
-  // indicating the function, and passing it an input
-  const addr = alice.call("my_zome", "create_my_entry", {"entry" : {"content":"sample content"}})
-  const result = alice.call("my_zome", "get_my_entry", {"address": addr.Ok})
+scenario.runTape('Can create a list', async (t, { alice }) => {
+  const createResult = await alice.callSync('lists', 'create_list', { list: { name: 'test list' } })
+  console.log(createResult)
+  t.notEqual(createResult.Ok, undefined)
+})
 
-  // check for equality of the actual and expected results
-  t.deepEqual(result, { Ok: { App: [ 'my_entry', '{"content":"sample content"}' ] } })
+scenario.runTape('Can add some items', async (t, { alice }) => {
+  const createResult = await alice.callSync('lists', 'create_list', { list: { name: 'test list' } })
+  const listAddr = createResult.Ok
+
+  const result1 = await alice.callSync('lists', 'add_item', { list_item: { text: 'Learn Rust', completed: true }, list_addr: listAddr })
+  const result2 = await alice.callSync('lists', 'add_item', { list_item: { text: 'Master Holochain', completed: false }, list_addr: listAddr })
+
+  console.log(result1)
+  console.log(result2)
+
+  t.notEqual(result1.Ok, undefined)
+  t.notEqual(result2.Ok, undefined)
+})
+
+scenario.runTape('Can get a list with items', async (t, { alice }) => {
+  const createResult = await alice.callSync('lists', 'create_list', { list: { name: 'test list' } })
+  const listAddr = createResult.Ok
+
+  await alice.callSync('lists', 'add_item', { list_item: { text: 'Learn Rust', completed: true }, list_addr: listAddr })
+  await alice.callSync('lists', 'add_item', { list_item: { text: 'Master Holochain', completed: false }, list_addr: listAddr })
+
+  const getResult = await alice.callSync('lists', 'get_list', { list_addr: listAddr })
+  console.log(getResult)
+
+  t.equal(getResult.Ok.items.length, 2, 'there should be 2 items in the list')
 })
--- zomes/lists/code/src/lib.rs
+++ zomes/lists/code/src/lib.rs
@@ -1,82 +1,142 @@
 #![feature(try_from)]
 #[macro_use]
 extern crate hdk;
-extern crate serde;
 #[macro_use]
 extern crate serde_derive;
-extern crate serde_json;
 #[macro_use]
 extern crate holochain_core_types_derive;
 
 use hdk::{
-    entry_definition::ValidatingEntryType,
     error::ZomeApiResult,
+    holochain_core_types::{
+        hash::HashString,
+        error::HolochainError,
+        dna::entry_types::Sharing,
+        json::JsonString,
+        cas::content::Address,
+        entry::Entry,
+    }
 };
-use hdk::holochain_core_types::{
-    cas::content::Address,
-    entry::Entry,
-    dna::entry_types::Sharing,
-    error::HolochainError,
-    json::JsonString,
-    validation::EntryValidationData
-};
 
-// see https://developer.holochain.org/api/0.0.17-alpha2/hdk/ for info on using the hdk library
-
-// This is a sample zome that defines an entry type "MyEntry" that can be committed to the
-// agent's chain via the exposed function create_my_entry
-
-#[derive(Serialize, Deserialize, Debug, DefaultJson,Clone)]
-pub struct MyEntry {
-    content: String,
-}
-
-pub fn handle_create_my_entry(entry: MyEntry) -> ZomeApiResult<Address> {
-    let entry = Entry::App("my_entry".into(), entry.into());
-    let address = hdk::commit_entry(&entry)?;
-    Ok(address)
-}
-
-pub fn handle_get_my_entry(address: Address) -> ZomeApiResult<Option<Entry>> {
-    hdk::get_entry(&address)
-}
-
-fn definition() -> ValidatingEntryType {
-    entry!(
-        name: "my_entry",
-        description: "this is a same entry defintion",
-        sharing: Sharing::Public,
-        validation_package: || {
-            hdk::ValidationPackageDefinition::Entry
-        },
-
-        validation: | _validation_data: hdk::EntryValidationData<MyEntry>| {
-            Ok(())
-        }
-    )
-}
-
+ 
 define_zome! {
     entries: [
-       definition()
+        entry!(
+            name: "list",
+            description: "",
+            sharing: Sharing::Public,
+            validation_package: || hdk::ValidationPackageDefinition::Entry,
+            validation: |validation_data: hdk::EntryValidationData<List>| {
+                Ok(())
+            },
+            links: [
+                to!(
+                    "listItem",
+                    link_type: "items",
+                    validation_package: || hdk::ValidationPackageDefinition::Entry,
+                    validation: |_validation_data: hdk::LinkValidationData| {
+                        Ok(())
+                    }
+                )
+            ]
+        ),
+        entry!(
+            name: "listItem",
+            description: "",
+            sharing: Sharing::Public,
+            validation_package: || hdk::ValidationPackageDefinition::Entry,
+            validation: |validation_data: hdk::EntryValidationData<ListItem>| {
+                Ok(())
+            }
+        )
     ]
-
-    genesis: || { Ok(()) }
-
-    functions: [
-        create_my_entry: {
-            inputs: |entry: MyEntry|,
-            outputs: |result: ZomeApiResult<Address>|,
-            handler: handle_create_my_entry
-        }
-        get_my_entry: {
-            inputs: |address: Address|,
-            outputs: |result: ZomeApiResult<Option<Entry>>|,
-            handler: handle_get_my_entry
-        }
-    ]
-
-    traits: {
-        hc_public [create_my_entry,get_my_entry]
+ 
+    genesis: || {
+        Ok(())
     }
+ 
+	functions: [
+        create_list: {
+            inputs: |list: List|,
+            outputs: |result: ZomeApiResult<Address>|,
+            handler: handle_create_list
+        }
+        add_item: {
+            inputs: |list_item: ListItem, list_addr: HashString|,
+            outputs: |result: ZomeApiResult<Address>|,
+            handler: handle_add_item
+        }
+        get_list: {
+            inputs: |list_addr: HashString|,
+            outputs: |result: ZomeApiResult<GetListResponse>|,
+            handler: handle_get_list
+        }
+    ]
+    traits: {
+        hc_public [create_list, add_item, get_list]
+    }
+}
+
+
+#[derive(Serialize, Deserialize, Debug, Clone, DefaultJson)]
+struct List {
+    name: String
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, DefaultJson)]
+struct ListItem {
+    text: String,
+    completed: bool
+}
+
+#[derive(Serialize, Deserialize, Debug, DefaultJson)]
+struct GetListResponse {
+    name: String,
+    items: Vec<ListItem>
+}
+
+fn handle_create_list(list: List) -> ZomeApiResult<Address> {
+    // define the entry
+    let list_entry = Entry::App(
+        "list".into(),
+        list.into()
+    );
+
+    // commit the entry and return the address
+	hdk::commit_entry(&list_entry)
+}
+
+
+fn handle_add_item(list_item: ListItem, list_addr: HashString) -> ZomeApiResult<Address> {
+    // define the entry
+    let list_item_entry = Entry::App(
+        "listItem".into(),
+        list_item.into()
+    );
+
+	let item_addr = hdk::commit_entry(&list_item_entry)?; // commit the list item
+	hdk::link_entries(&list_addr, &item_addr, "items", "tag")?; // if successful, link to list address
+	Ok(item_addr)
+}
+
+
+fn handle_get_list(list_addr: HashString) -> ZomeApiResult<GetListResponse> {
+
+    // load the list entry. Early return error if it cannot load or is wrong type
+    let list = hdk::utils::get_as_type::<List>(list_addr.clone())?;
+
+    // try and load the list items, filter out errors and collect in a vector
+    let list_items = hdk::get_links(&list_addr, Some("items".to_string()), Some("tag".to_string()))?.addresses()
+        .iter()
+        .map(|item_address| {
+            hdk::utils::get_as_type::<ListItem>(item_address.to_owned())
+        })
+        .filter_map(Result::ok)
+        .collect::<Vec<ListItem>>();
+
+    // if this was successful then return the list items
+    Ok(GetListResponse{
+        name: list.name,
+        items: list_items
+    })
 }
